cd2a91ee58832db8aef47a73ff3114f861f3722c
adrsamanta <alan.samanta@gmail.com>
2015-06-01 13:01:41 -0400

    Added All Files

---
+++ app/src/main/java/com/samanta/alan/counter/CounterFragment.java
@@ -0,0 +1,159 @@
+package com.samanta.alan.counter;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CounterFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CounterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CounterFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    // TODO: Rename and change types of parameters
+    private int count;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CounterFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CounterFragment newInstance(String param1, String param2) {
+        CounterFragment fragment = new CounterFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public CounterFragment() {
+        count=0;
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d("CounterFrag", "Creating CounterFrag");
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=  inflater.inflate(R.layout.fragment_counter, container, false);
+
+        //find textfield, set to counter value
+        EditText field= (EditText) v.findViewById(R.id.counter_Field);
+        field.setText(Integer.toString(count));
+        //attach text changed listener to field
+        field.addTextChangedListener(new countChangedListener());
+        return v;
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            Log.e("CounterFrag", "Couldn't case main activity to OnFragmentInteractionListener");
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public void decrement(View view){
+        Log.d("CounterFrag", "Decremented Count");
+        --count;
+        updateFieldFromCount();
+    }
+
+    public void increment(View view){
+        Log.d("CounterFrag", "Incremented Count");
+        ++count;
+        updateFieldFromCount();
+    }
+
+    private class countChangedListener implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Log.d("CounterFrag", "Count Changed Listener Fired");
+            try{
+                int newCount=Integer.parseInt(s.toString());
+                count=newCount;
+            }
+            catch (NumberFormatException e){
+                Log.e("CounterFrag", "NumberFormatException");
+                //Eek....
+                //?
+            }
+        }
+    }
+
+    private void updateFieldFromCount(){
+        EditText field= (EditText) getView().findViewById(R.id.counter_Field);
+        field.setText(Integer.toString(count));
+    }
+
+    public void close(View view){
+        mListener.closeThis(this);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void closeThis(CounterFragment toClose);
+    }
+
+}
---
+++ app/src/main/java/com/samanta/alan/counter/MainActivity.java
@@ -0,0 +1,62 @@
+package com.samanta.alan.counter;
+
+//import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Fragment;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity implements CounterFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        newCounter();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void closeThis(CounterFragment toClose) {
+        Log.d("MainActivity", "Closing CounterFragment");
+        getSupportFragmentManager().beginTransaction().detach(toClose).commit();
+    }
+
+    public void newCounter(View view){
+       Log.d("MainActivity", "newCounter method");
+        newCounter();
+    }
+
+    private void newCounter(){
+        Log.d("MainActivity", "Adding New Counter");
+        FragmentTransaction addCount = getSupportFragmentManager().beginTransaction();
+        addCount.add(R.id.counter_layout, new CounterFragment());
+        addCount.commit();
+    }
+}
