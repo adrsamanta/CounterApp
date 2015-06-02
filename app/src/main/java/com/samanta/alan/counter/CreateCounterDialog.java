85bc153dad64f67e85edc7f919daec79789aa932
adrsamanta <alan.samanta@gmail.com>
2015-06-02 15:29:35 -0400

    working to fix number picker

--- app/src/main/java/com/samanta/alan/counter/CreateCounterDialog.java
+++ app/src/main/java/com/samanta/alan/counter/CreateCounterDialog.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 
@@ -46,13 +47,20 @@ public class CreateCounterDialog extends DialogFragment {
                 R.layout.create_counter, null);
         builder.setView(myView);
 
+		final NumberPicker sv= (NumberPicker) myView.findViewById(R.id.start_Val_Picker);
+		sv.setMinValue(0);
+		sv.setMaxValue(10000);
+
         builder.setPositiveButton(R.string.create_counter,
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         EditText titleBox = (EditText) myView.findViewById(R.id.title_Text);
-                        NumberPicker sv= (NumberPicker) myView.findViewById(R.id.start_Val_Picker);
-                        myCC.newCounter(titleBox.getText().toString(), sv.getValue());
+						CheckBox checkBox = (CheckBox) myView.findViewById(R.id.negCheckBox);
+
+						int val= checkBox.isChecked() ? -1*sv.getValue() : sv.getValue();
+
+                        myCC.newCounter(titleBox.getText().toString(), val);
                     }
                 });
 
@@ -64,4 +72,26 @@ public class CreateCounterDialog extends DialogFragment {
         });
         return builder.create();
     }
+
+	/**
+	 * Sets up the given number picker to have the correct values
+	 * currently max/min set at 10000 arbitrarily
+	 * @param np number picker to be set up
+	 */
+	//No longer used!
+	private void setUpNumberPicker(NumberPicker np){
+		final int minVal=-10000;
+		final int maxVal=10000;
+
+		np.setMinValue(0);
+		np.setMaxValue(maxVal-minVal);
+		np.setValue(10000);
+
+		np.setFormatter(new NumberPicker.Formatter() {
+			@Override
+			public String format(int value) {
+				return Integer.toString(value+minVal);
+			}
+		});
+	}
 }
